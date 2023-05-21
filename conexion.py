c2eac8a3dc08d96802b8e171762b2beb985b7d24
HebeV <hebesoledadvelazquez@gmail.com>
2023-05-21 20:04:19 -0300

    Análisis caso Ley 25326

--- conexion.py
+++ conexion.py
@@ -1,19 +1,20 @@
 import mysql.connector 
-from mysql.connector import Error
+#from mysql.connector import Error
 
-class Conectar_Base_de_Datos():
-    def __init__(self):
-        try:
-            print("Hol Mundo")
-            self.conexion = mysql.connector.connect(
+#class Conectar_Base_de_Datos():
+    #def __init__(self):
+        #try:
+            #print("Hol Mundo")
+conexion = mysql.connector.connect(
                 host= 'localhost',
                 port= 3306,
                 user= 'root',
-                password= '2468vale',
-                db='bd_ejemplo_innova',
+                password= '',
+                db='bd_ejemplo_innova'
             )
-            print("Hol Mundo")
-        except mysql.connector.Error as descripcionError:
-            print("No se conecto la base de datos", descripcionError)
+print("Conexion exitosa")
+        #except mysql.connector.Error as descripcionError:
+           #print("No se conecto la base de datos", descripcionError)
+
 
 
