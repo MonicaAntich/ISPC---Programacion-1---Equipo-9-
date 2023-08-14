68897df943a885b75852ba89b145a8f95454dd33
HebeV <hebesoledadvelazquez@gmail.com>
2023-08-14 18:55:23 -0300

    Evidencia7 Hebe

---
+++ Archivos PDF/Evidencia 7/Evidencia7 - Hebe Velazquez.py
@@ -0,0 +1,6 @@
+#Ejercicio N° 1
+familia_Velazquez=["Camilo", "Sofia", "Hebe"]
+temperatura_marzo23 = [     "lunes 1 de marzo 23 grados","martes 2 de marzo 25 grados","miércoles 3 de marzo 27 grados","jueves 4 de marzo 28 grados", "viernes 5 de marzo 25 grados","sábado 6 de marzo 27 grados","domingo 7 de marzo"]
+#print(temperatura_marzo23)
+ciudades_visitadas = ["Buenos Aires", "Santa Fe" "Tucumán"]
+#print(familia_Velazquez)
