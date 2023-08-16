11e5e90dc21c2a451c3374469e974cdaa6e06f0a
Monica <moniantich@gmail.com>
2023-08-15 23:24:14 -0300

    condicional if-elif-else

--- Archivos PDF/Ejercicios2do cuatrimeste/ejerc_estruc_condicionales_Antich.py
+++ Archivos PDF/Ejercicios2do cuatrimeste/ejerc_estruc_condicionales_Antich.py
@@ -4,22 +4,22 @@ init(autoreset=True)
 
 #1. Pedirle al usuario que ingrese un número, si este es 10 se debe imprimir:
 #  ¡Usted ha ganado el sorteo!
-# numero = int(input("Ingrese un numero: "))
-# if numero == 10:
-#     print("Usted ha ganado el sorteo")
+numero = int(input("Ingrese un numero: "))
+if numero == 10:
+    print("Usted ha ganado el sorteo")
 
-# print(Fore.GREEN+Style.BRIGHT+"""\n****************************************************""")
+print(Fore.GREEN+Style.BRIGHT+"""\n****************************************************""")
 
 #2. Sumar al ejercicio anterior, una pregunta: Si el número es menor imprimir: ¡Falto un poco,
 #seguí participando! Si es mayor, imprimir: ¡Te pasaste, a seguir intentando!
 
-# numero = int(input("Ingrese un numero: "))
-# if numero == 10:
-#     print("Usted ha ganado el sorteo")
-# elif numero < 10:
-#     print("¡Falto un poco, seguí participando!")
-# else :
-#     print("¡Te pasaste, a seguir intentando!")
+numero = int(input("Ingrese un numero: "))
+if numero == 10:
+    print("Usted ha ganado el sorteo")
+elif numero < 10:
+    print("¡Falto un poco, seguí participando!")
+else :
+    print("¡Te pasaste, a seguir intentando!")
 
 print(Fore.GREEN+Style.BRIGHT+"""\n****************************************************""")
 
@@ -28,17 +28,15 @@ print(Fore.GREEN+Style.BRIGHT+"""\n*********************************************
 #ingresado no es ninguno de esos, imprimir otro mensaje.
 dia_semana = (input("Ingrese un dia de la semana: "))
 if dia_semana == "lunes":
-    print("Lunes, a trabajar")
+    print("Lunes, a trabajar" )
 elif dia_semana == "viernes":
     print("usted ingreso viernes, ultimo dia de la semana")
 elif dia_semana == "sabado":
     print ("Usted ingreso sabado, y el cuerpo lo sabe")
 elif dia_semana == "domingo":
     print("Usted ingreso domingo, a disfrutar con la familia") 
-elif dia_semana == "martes" or "miercoles" or "jueves":
-    print("Usted no ingreso un dia valido")       
 else :
-     print("Usted no ingreso un dia valido")
+    print("Usted no ingreso un dia valido")
 
 print(Fore.GREEN+Style.BRIGHT+"""\n****************************************************""")
 
@@ -49,6 +47,6 @@ letra = (input("Ingrese una letra: "))
 if letra in ["a", "e", "i", "o","u"]:
     print("Usted ingreso una vocal", letra) 
 else:
-    print("Usted ingreso una consonante", letra)   
+    print("Usted ingreso una consonante: ", letra, " ¡debe ingresar una vocal!")   
 
 print(Fore.GREEN+Style.BRIGHT+"""\n****************************************************""")    
